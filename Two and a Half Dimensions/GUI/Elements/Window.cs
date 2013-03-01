8b2ac49987a6a440addf4a43055c334250489c84
Foohy <swkauker@yahoo.com>
2013-02-28 16:27:34 -0800

    Added better mouse resizing to window
    Also added window.MaximumSize/MinimumSize
    Setting the mouse cursor for resizing the window

--- Two and a Half Dimensions/GUI/Elements/Window.cs
+++ Two and a Half Dimensions/GUI/Elements/Window.cs
@@ -10,14 +10,29 @@ namespace Two_and_a_Half_Dimensions.GUI
 {
     class Window : Panel
     {
+        private enum ResizeMode
+        {
+            None,
+            Top,
+            Left,
+            Right,
+            Bottom,
+            TopLeft,
+            TopRight,
+            BottomLeft,
+            BottomRight
+        }
         public string WindowTitle { get; private set; }
+        public bool Resizable { get; set; } //Should the mouse be able to size it?
+        public Vector2 MinimumSize { get; set; } //Minimum size of the window
+        public Vector2 MaximumSize { get; set; } //Maximum size of the window
 
         Panel Title;
         Label TitleText;
         Button closeButton;
         Vector2 Offset = Vector2.Zero;
+        ResizeMode CurrentResizeMode = ResizeMode.None;
         bool dragging = false;
-        bool resizing = false;
 
         public Window()
         {
@@ -67,9 +82,6 @@ namespace Two_and_a_Half_Dimensions.GUI
         void closeButton_OnButtonPress(Panel sender)
         {
             this.Remove();
-            closeButton.Remove();
-            TitleText.Remove();
-            Title.Remove();
         }
 
         void Title_OnMouseUp(Panel sender, OpenTK.Input.MouseButtonEventArgs e)
@@ -103,9 +115,30 @@ namespace Two_and_a_Half_Dimensions.GUI
             base.MouseDown(e);
             this.SendToFront();
             Title.SendToFront();
-            if (this.MouseWithinCorner())
+
+            if (this.MouseWithinBottom())
             {
-                this.resizing = true;
+                this.CurrentResizeMode = ResizeMode.Bottom;
+            }
+
+            if (this.MouseWithinRight())
+            {
+                this.CurrentResizeMode = ResizeMode.Right;
+            }
+
+            if (this.MouseWithinLeft())
+            {
+                this.CurrentResizeMode = ResizeMode.Left;
+            }
+
+
+            if (this.MouseWithinRightCorner())
+            {
+                this.CurrentResizeMode = ResizeMode.BottomRight;
+            }
+            if (this.MouseWithinLeftCorner())
+            {
+                this.CurrentResizeMode = ResizeMode.BottomLeft;
             } 
         }
 
@@ -113,11 +146,29 @@ namespace Two_and_a_Half_Dimensions.GUI
         {
             base.MouseMove(e);
 
-            if (this.resizing)
+            if (this.MouseWithinBottom() )
             {
-                Vector2 Screenpos = this.GetScreenPos();
-                this.SetWidth(Utilities.Clamp(Utilities.window.Mouse.X - Screenpos.X, 10000, this.TitleText.GetTextLength() + closeButton.Width));
-                this.SetHeight( Utilities.Clamp( Utilities.window.Mouse.Y - Screenpos.Y, 10000, 30 ));
+                System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.SizeNS;
+            }
+
+            if (this.MouseWithinRight() || this.MouseWithinLeft())
+            {
+                System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.SizeWE;
+            }
+
+            if (this.MouseWithinRightCorner())
+            {
+                System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.SizeNWSE;
+            }
+
+            if (this.MouseWithinLeftCorner())
+            {
+                System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.SizeNESW;
+            }
+            
+            if (CurrentResizeMode != ResizeMode.None )
+            {
+                ResizeThink();
 
                 Title.SetWidth(this.Width);
             }
@@ -125,9 +176,10 @@ namespace Two_and_a_Half_Dimensions.GUI
 
         public override void MouseUp(OpenTK.Input.MouseButtonEventArgs e)
         {
-            if (this.resizing)
+            if (CurrentResizeMode != ResizeMode.None)
             {
-                this.resizing = false;
+                ResizeThink();
+                this.CurrentResizeMode = ResizeMode.None;
 
                 Title.SetWidth(this.Width);
             }
@@ -142,8 +194,21 @@ namespace Two_and_a_Half_Dimensions.GUI
 
         public override void Resize()
         {
+            //Make sure our size is within our minimum and maximum
+            this.Width = Utilities.Clamp(this.Width, (this.MaximumSize.X <= 0) ? float.PositiveInfinity : this.MaximumSize.X, (this.MinimumSize.X < this.closeButton.Width) ? this.closeButton.Width : this.MinimumSize.X);
+            this.Height = Utilities.Clamp(this.Height, (this.MaximumSize.Y <= 0) ? float.PositiveInfinity : this.MaximumSize.Y, this.MinimumSize.Y);
+
             base.Resize();
             Title.SetWidth(this.Width);
+        }
+
+        public override void Remove()
+        {
+            base.Remove();
+
+            closeButton.Remove();
+            TitleText.Remove();
+            Title.Remove();
         }
 
         public void SetTitle(string str)
@@ -153,16 +218,102 @@ namespace Two_and_a_Half_Dimensions.GUI
         }
 
         private static int size = 10; //must be within a box of this many pixels wide
-        private bool MouseWithinCorner()
+        private bool MouseWithinRightCorner()
         {
-            Vector2 MousePos = new Vector2(Utilities.window.Mouse.X, Utilities.window.Mouse.Y);
-            Vector2 CornerPos = this.GetScreenPos() + new Vector2( this.Width, this.Height );
-            if (MousePos.X < CornerPos.X - size) return false;
-            if (MousePos.X > CornerPos.X + size) return false;
-            if (MousePos.Y < CornerPos.Y - size) return false;
-            if (MousePos.Y > CornerPos.Y + size) return false;
+            return (MouseWithinBottom() && MouseWithinRight());
+        }
 
-            return true;
+        private bool MouseWithinLeftCorner()
+        {
+            return (MouseWithinBottom() && MouseWithinLeft());
+        }
+
+        private bool MouseWithinBottom()
+        {
+            if (this.IsMouseOver())
+            {
+                if ((this.GetScreenPos().Y + this.Height) - Utilities.window.Mouse.Y < size)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool MouseWithinRight()
+        {
+            if (this.IsMouseOver())
+            {
+                if ((this.GetScreenPos().X + this.Width) - Utilities.window.Mouse.X < size)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool MouseWithinTop()
+        {
+            if (this.IsMouseOver())
+            {
+                if ((Utilities.window.Mouse.Y - this.GetScreenPos().Y)  < size)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool MouseWithinLeft()
+        {
+            if (this.IsMouseOver())
+            {
+                if ((Utilities.window.Mouse.X - this.GetScreenPos().X) < size)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        //Handle resizing by mouse
+        private void ResizeThink()
+        {
+            if (CurrentResizeMode == ResizeMode.None) return;
+            Vector2 Screenpos = this.GetScreenPos();
+
+            if (CurrentResizeMode == ResizeMode.TopLeft ||
+            CurrentResizeMode == ResizeMode.Left ||
+            CurrentResizeMode == ResizeMode.BottomLeft)
+            {
+                this.SetWidth((this.Position.X + this.Width) - Utilities.window.Mouse.X );
+                this.SetPos(Utilities.window.Mouse.X, this.Position.Y);
+            }
+
+            if (CurrentResizeMode == ResizeMode.TopRight ||
+            CurrentResizeMode == ResizeMode.Right ||
+            CurrentResizeMode == ResizeMode.BottomRight)
+            {
+                this.SetWidth(Utilities.window.Mouse.X - Screenpos.X);
+            }
+
+            if (CurrentResizeMode == ResizeMode.TopLeft ||
+            CurrentResizeMode == ResizeMode.Top ||
+            CurrentResizeMode == ResizeMode.TopRight)
+            {
+                this.SetPos(this.Position.Y, Utilities.window.Mouse.Y);
+            }
+
+            if (CurrentResizeMode == ResizeMode.BottomLeft ||
+            CurrentResizeMode == ResizeMode.Bottom ||
+            CurrentResizeMode == ResizeMode.BottomRight)
+            {
+                this.SetHeight(Utilities.window.Mouse.Y - Screenpos.Y);
+            }
         }
 
         public override void Draw()
